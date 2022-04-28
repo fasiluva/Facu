5adeb08b696e88ca589b5b3af26f8e4a1ef7c10c
Valenchu <68815558+fasiluva@users.noreply.github.com>
2022-04-28 10:24:38 -0300

    Update Sandbox.rkt

--- Facu-main/Sandbox.rkt
+++ Facu-main/Sandbox.rkt
@@ -4,6 +4,86 @@
 ; Sandbox:
 
 
+#|
+(define COLOR "yellow")
+(define ALTO 300)
+(define LARGO 300)
+(define RADIO 50)
+(define ESCENARIO (empty-scene LARGO ALTO))
+
+(define (interpretar n) (place-image (circle RADIO "solid" n) (/ LARGO 2) (/ ALTO 2)
+                                     ESCENARIO))
+
+(define (cambiar n) (cond [(string=? "yellow" n) "red"]
+                          [(string=? "red" n) "green"]
+                          [(string=? "green" n) "blue"]
+                          [else "yellow"]))
+
+(big-bang COLOR
+  [to-draw interpretar]
+  [on-tick cambiar 0.5] ; CANTIDAD DE SEGUNDOS 
+  )
+
+
+
+; (text <string> <font-size> <color>)
+; (place-image/align <image> <x> <y> <x-place> <y-place> <scene>)
+;                      |(figura-geometrica <largo,ancho> <solid> <color>)
+; <x> <y>: coordenadas en basa al escenario sobre el que se posiciona
+; <x-place> <y-place>: que punto de la <image> toma como referencia. Ejemplo: "center" toma el centro de la figura
+
+(define LARGO 800)
+(define ANCHO 60)
+(define ESCENARIO (empty-scene 800 60))
+(define CADENA "")
+;(define last-char-position (string-ith CADENA (- (string-length CADENA) 1)))
+
+(define (interpretar CADENA) (place-image/align (text CADENA 20 "indigo")
+                                                0 0
+                                                "left" "top"
+                                                ESCENARIO))
+(define (escribir CADENA k) (cond [(key=? k "\b")
+                                   (if (> (string-length CADENA) 0)
+                                       (substring CADENA 0 (- (string-length CADENA) 1))
+                                       CADENA)]
+                                  [(string? k) (string-append CADENA k)]
+                                  [else CADENA]))
+
+(big-bang CADENA
+  [to-draw interpretar]
+  [on-key escribir])
+|#
+
+(define AUTO .)
+(define LARGO 800)
+(define ALTO 150)
+(define ESCENARIO (empty-scene LARGO ALTO))
+(define estadoInicial 0)
+(define estadoCambiante 0)
+
+(define (interpretar movimiento) (place-image/align
+                                  AUTO
+                                  movimiento (/ ALTO 2)
+                                  "right" "center"
+                                  ESCENARIO)
+  )
+
+(define (mover n) (cond [(<= n LARGO) (+ n 3)]
+                        [else n]
+                        )
+  )
+
+(define (reiniciar estadoCambiante k)(cond [(key=? k " ") estadoInicial]
+                                   [else estadoCambiante]))
+
+(define (posicionar estadoCambiante x y evento) (cond [(string=? evento "button-down") x]
+                                                      [else estadoCambiante]))
+
+(big-bang estadoCambiante
+  [to-draw interpretar]
+  [on-tick mover]
+  [on-key reiniciar]
+  [on-mouse posicionar])
 
 
 
