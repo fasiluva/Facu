7e21a87e4d6c009d36569ac3afbcb37d30984a0e
Valenchu <68815558+fasiluva@users.noreply.github.com>
2022-05-03 23:21:05 -0300

    Update Sandbox.rkt

--- Facu-main/Sandbox.rkt
+++ Facu-main/Sandbox.rkt
@@ -5,8 +5,8 @@
 ; Escribir un programa que muestre una pelota que pica
 ; continuamente modificando su forma a una elipse
 ; al llegar a un extremo.
-(define ALTO 300)
-(define ANCHO 500)
+(define ALTO 600)
+(define ANCHO 100)
 (define RADIO 40)         ;este es el radio del círculo
 (define POSY-MIN RADIO)   ;coordenada mínima admitida sobre
 ; el eje y para q el centro de la elipse no se salga
@@ -16,17 +16,9 @@
 ; salga de la escena
 (define DELTA 15)
 
-
 ; marco donde picará la pelota
 (define MARCO
-        (empty-scene ANCHO ALTO))
-
-; imagen de ejemplo
-(define IMAGEN0 
-   (place-image (ellipse 15 30 "solid" "blue")
-                (/ ANCHO 2) ; coordenada x
-                RADIO ; coordenada y 
-                MARCO) )
+  (empty-scene ANCHO ALTO))
 
 ; Estado : (coordenada y , sentido del movimiento)
 
@@ -37,12 +29,8 @@
 ; Estado inicial
 (define INICIAL (string-append "s" (number->string POSY-MAX)))
 
-
 ; coordY : Estado -> Number
 ; La función obtiene la coordenada Y a partir del estado
-
-(check-expect (coordY "s40") 40)
-(check-expect (coordY "b200") 200)
 
 (define (coordY s)
         (string->number (substring s 1 (string-length s))) )
@@ -58,18 +46,10 @@
          (coordY s) ; coordenada y 
          MARCO))
 
-
-;(big-bang INICIAL
-;          [to-draw interpretar])
-
 ; Funciones auxiliares
 ; cambiarPos : Estado String -> Estado
 ; Dado un estado y la nuevo posición, cambia el estado
 ; con la posición dada.
-(check-expect (cambiarPos "b200" "s") "s200")
-(check-expect (cambiarPos "s100" "b") "b100")
-(check-expect (cambiarPos "a200" "s") "s200")
-
 (define (cambiarPos s p)
     (string-append p (substring s 1 (string-length s))))
 
@@ -79,27 +59,12 @@
 ; se mueve la pelota ("s" o "b") arma un estado
 ; con ellos.
 
-(check-expect (armaEstado "b" 300) "b300")
-
 (define (armaEstado sentido y)
    (string-append sentido (number->string y)))
 
 ; avanza-reloj : Estado -> Estado
 ; Dado el estado del reloj modifica el estado dependiendo
 ; de si la pelota está subiendo o bajando.
-
-(check-expect (avanza-reloj "s100")
-     (armaEstado "s" (- 100 DELTA)))
-
-(check-expect (avanza-reloj "b100")
-     (armaEstado "b" (+ 100 DELTA)))
-
-(check-expect (avanza-reloj "s20") ; cambia el sentido de "s" a "b"
-     (armaEstado "b" 20))
-
-(check-expect (avanza-reloj "b260")   ; cambia el sentido de "b" a "s"
-     (armaEstado "s" 260))
-
 
 (define (avanza-reloj s)
      (if (string=? (string-ith s 0) "b")
@@ -111,7 +76,6 @@
         (cond [(<= (- (coordY s) DELTA) POSY-MIN) 
                    (cambiarPos s "b")]
               [else (armaEstado "s" (- (coordY s) DELTA))])))  
-
 
 (big-bang INICIAL
           [to-draw interpretar]
