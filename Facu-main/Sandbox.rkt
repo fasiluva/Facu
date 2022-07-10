8199fd78d140781d70069abf65a7a43e044d8010
Valenchu <68815558+fasiluva@users.noreply.github.com>
2022-07-10 16:00:11 -0300

    Update Sandbox.rkt

--- Facu-main/Sandbox.rkt
+++ Facu-main/Sandbox.rkt
@@ -3,119 +3,6 @@
 #reader(lib "htdp-intermediate-reader.ss" "lang")((modname Sandbox) (read-case-sensitive #t) (teachpacks ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp"))) (htdp-settings #(#t constructor repeating-decimal #f #t none #f ((lib "universe.rkt" "teachpack" "2htdp") (lib "image.rkt" "teachpack" "2htdp")) #f)))
 ; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
-; (define (funcion <predicado> <lista>) (<patron> <predicado> <lista>))
-
-; (filter <predicado> <lista>) = Se usa para filtar los elementos que se desean de
-; una lista. Puede devolver menos cantidad de elementos de una lista, pero nunca mas.
-
-; (map <predicado> <lista>) = Se usa para aplicar una transformacion a los elementos
-; de una lista. Siempre devuelve la misma cantidad de elementos que la lista posee.
-
-; (foldr <predicado> <caso base> <lista>) = Se usa para operar todos los elementos de una lista.
-; Devuelve siempre un tipo de dato diferente a una lista, como un Number o String.
-
-; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-; Ejercicio 6. Diseñe la función raices, que dada una lista de números,
-; devuelve una lista con las raíces cuadradas de sus elementos.
-
-(define (raices0 l) (cond [(empty? l) '()]
-                          [(cons? l) (cons (sqrt (first l)) (raices0 (rest l)))]))
-
-(check-expect (raices0 (list 0 1 4 16)) (list 0 1 2 4))
-
-(define LISTA5 (list 0 1 4 16))
-
-(define (raiz x) (sqrt x))
-(define (raices pred l)(map pred l))
-
-(check-expect (raices raiz LISTA5) (list 0 1 2 4))
-
-; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-; Ejercicio 7. Diseñe una función distancias que tome una lista de
-; puntos del plano y devuelva una lista con la distancia al origen de cada
-; uno.
-
-(define LISTA6 (list (make-posn 3 4) (make-posn 0 4) (make-posn 12 5))) ; 5 4 13
-
-(define (dist-origen a) (sqrt (+ (sqr (+ (posn-x a) 0)) (sqr (+ (posn-y a) 0)))))
-
-(define (distancias pred l) (map pred l))
-
-(check-expect (distancias dist-origen LISTA6) (list 5 4 13))
-
-; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-; Ejercicio 8. Diseñe una función anchos que tome una lista de imágenes
-; y devuelva una lista con el ancho de cada una
-
-(define LISTA7 (list (circle 5 "solid" "red") (rectangle 10 20 "solid" "blue") (rectangle 20 5 "solid" "grey"))) ; 10 20 5
-
-(define (ancho-img x) (image-height x))
-
-(define (anchos pred l) (map pred l))
-
-(check-expect (anchos ancho-img LISTA7) (list 10 20 5))
-
-; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-; Ejercicio 13. Diseñe una función prod que multiplica los elementos de
-; una lista de números. Para la lista vacía, devuelve 1.
-
-(define LISTA8 (list 1 2 3 4 5)) ; 120
-
-;(foldr * 1 LISTA8)
-
-; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-; Ejercicio 14. Diseñe una función pegar que dada una lista de strings,
-; devuelve el string que se obtiene de concatenar todos los elementos de
-; la lista.
-
-(define LISTA9 (list "Mr." "Robot" " es" " una" " serie" " muy" " buena."))
-
-(define (pegar l) (foldr string-append "" l))
-
-;(pegar LISTA9)
-
-; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-(define LISTA10 (list 20 21 25 50 1))
-(define LISTA11 (list 1 2 5 9 100))
-
-(define (smax l) (foldr max 0 l))
-
-;(smax LISTA10)
-;(smax LISTA11)
-
-; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
-(check-expect (sumcuad empty) 0)
-(check-expect (sumcuad (list 1 2 3)) 14)
-(check-expect (sumcuad (list -1 -2)) 5)
-
-(define (sumcuad l)
-  (foldr + 0 (map sqr l)))
-
-<<<<<<< HEAD
-; Ejercicio 11. Diseñe una función longitudes que tome una lista de
-; cadenas y devuelva una lista de números que corresponda con la
-; longitud de cada cadena de la lista original.
-
-(define (longitudes0 l) (cond [(empty? l) empty]
-                              [(cons? l) (cons (string-length (first l)) (longitudes0 (rest l)))]))
-
-(check-expect (longitudes0 (list "hola" "si")) (list 4 2))
-
-(define (predicade x) (string-length x))
-
-(define (longitudes l) (map predicade l))
-
-(check-expect (longitudes (list "hola" "como" "estas?")) (list 4 4 6))
-=======
-; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
-
 ; Pedido [String Number Number]
 ; menu = 1,2,3
 ; desert = 1,2
@@ -146,13 +33,116 @@
  
 ; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
 
+; peso / (altura^2) = IMC
+; IMC /2 = Promedio
+
+(define-struct Pers[peso altura])
+; Pers[Number Number]
+
+(define (IMC s) (/ (Pers-peso s) (sqr (Pers-altura s))))
+
+(define (prom-imc l) (promedio (sum-IMC l) (cant-elementos l)))
+
+(define (cant-elementos l) (cond [(empty? l) 0]
+                                 [(cons? l) (+ 1 (cant-elementos (rest l)))]))
+
+(define (sum-IMC l) (cond [(empty? l) 0]
+                          [(cons? l) (+ (IMC (first l)) (sum-IMC (rest l)) )]))
+
+(define (promedio x n) (/ x n))
+
+(check-within (prom-imc (list (make-Pers 44 1.55) (make-Pers 50 1.56))) 19.43 0.01)
+
+;/////////////////////////////////////////////////////////////////////////////////////
+
+(define (clasif l) (cond [(empty? l) empty]
+                         [(cons? l) (cons (clasif-IMC (IMC (first l))) (clasif (rest l)))]))
+
+(define (clasif-IMC IMC) (cond [(< IMC 19.5) "Bajo peso"]
+                               [(< IMC 25) "Peso normal"]
+                               [(< IMC 30) "Sobrepeso"]
+                               [(>= IMC 30) "Obesidad"]))
+
+(check-expect (clasif (list (make-Pers 44 1.55) (make-Pers 50 1.56))) (list "Bajo peso" "Peso normal"))
+
+;/////////////////////////////////////////////////////////////////////////////////////
+
+(define (poblacionSaludable? l) (if (>= (saludables (clasif l) (cant-elementos l)) 0.8) #t #f))
+
+(define (saludables l n) (/ (cant-saludables l) n))
+
+(define (cant-saludables l) (cond [(empty? l) 0]
+                                  [(cons? l) (+ (saludable? (first l)) (cant-saludables (rest l)))]))
+
+(define (saludable? p) (if (string=? p "Peso normal") 1 0))
+
+(check-expect (poblacionSaludable? (list (make-Pers 44 1.55) (make-Pers 50 1.56))) #f)
+
+;/////////////////////////////////////////////////////////////////////////////////////
+
+(define-struct cuad[color angle rotation])
+; cuad[String Number Boolean]
+
+(define LARGO 400)
+(define ALTO 400)
+(define ESCENARIO (empty-scene LARGO ALTO "black"))
+(define LADO 50)
+(define estadoInicial (make-cuad "red" 0 #t))
+; ---------------------------------------------------------------------------
+#|
+(define (render estado) (place-image
+                         (rotate (cuad-angle estado) (square LADO "solid" (cuad-color estado)))
+                         (/ LARGO 2) (/ ALTO 2)
+                         ESCENARIO))
+
+(define (changeColor estado k) (cond [(key=? k " ") (make-cuad (cuad-color estado) (cuad-angle estado) (rotation? (cuad-rotation estado)))]
+                                     [(key=? k "b") (make-cuad "blue" (cuad-angle estado) (cuad-rotation estado))]
+                                     [(key=? k "r") (make-cuad "red" (cuad-angle estado) (cuad-rotation estado))]
+                                     [else estado]))
+
+(define (rotation? b) (if (boolean=? b #t) #f #t))
+
+(define (rotation estado) (if (boolean=? (cuad-rotation estado) #f)
+                              (make-cuad (cuad-color estado) (cuad-angle estado) #f)
+                              (if (>= (+ (cuad-angle estado) 5) 360)
+                                  (make-cuad (cuad-color estado) (- (+ 5 (cuad-angle estado)) 360) #t)
+                                  (make-cuad (cuad-color estado) (+ (cuad-angle estado) 5) #t))))
+
+(big-bang estadoInicial
+  [to-draw render]
+  [on-tick rotation]
+  [on-key changeColor]
+  )
+|#
+
+; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+(define-struct Book[isbn name writer rate])
+; Book[Any String String Number(1-10)]
+
+(define user1 (list (make-Book 02 "Sex" "Valentin Sosa" 10) (make-Book 69 "Anal 2" "Valentin Sosa" 9)))
+(define user2 (list (make-Book 02 "Sex" "Valentin Sosa" 1) (make-Book 69 "Anal 2" "Valentin Sosa" 1)))
+(define user3 (list (make-Book 02 "Sex" "Valentin Sosa" 8) (make-Book 69 "Anal 2" "Valentin Sosa" 8)))
+
+(define (coincide? b1 b2) (if (< (diferencia (Book-rate b1) (Book-rate b2)) 2) "La lectura es coincidente" "La lectura no es coincidente"))
+
+(define (diferencia r1 r2) (abs (- r1 r2)))
+
+(check-expect (coincide? (make-Book 0 "s" "s" 10) (make-Book 0 "s" "s" 9)) "La lectura es coincidente")
+(check-expect (coincide? (make-Book 0 "s" "s" 10) (make-Book 0 "s" "s" 5)) "La lectura no es coincidente")
+
+; \\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\\
+
+(define (suma-positivos-es-mayor? l) (>=
+                                      (abs (foldr + 0 (filter positive? (filter number? l))))
+                                      (abs (foldr + 0 (filter positive? (filter number? l))))
+                                      )
+  )
+
+(check-expect (suma-positivos-es-mayor? (list 5 "abc" 2 #t -3 "def")) #t)
+(check-expect (suma-positivos-es-mayor? (list (circle 10 "solid" "red") 12 1 -2 -45)) #t)
 
 
-
-
-
-
->>>>>>> 1adaa9facd999c6bcdea4dd15b75fb027c2827c1
 
 
 
