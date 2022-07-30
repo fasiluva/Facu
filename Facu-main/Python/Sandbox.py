bc1239220b731dc144ac000d27cfd1f4a97054a4
Valenchu <68815558+fasiluva@users.noreply.github.com>
2022-07-30 01:05:31 -0300

    j

--- Facu-main/Python/Sandbox.py
+++ Facu-main/Python/Sandbox.py
@@ -4,6 +4,8 @@ import SandboxModule as sm
 import tkinter as tk
 from tkinter import ttk
 
+# ---------------------------------------------------------------------------
+
 root = tk.Tk() # Crea la ventana principal. El contenido de la misma es asignado mediante root.<propiedad>
 
 def convertir_temp():
@@ -17,8 +19,12 @@ def convertir_temp():
     # Dato: f"string" se usa como .format(). Implementar.
     etiqueta_temp_fahrenheit.config(text=f"Temperatura en ºF: {temp_fahrenheit}")
 
+# ---------------------------------------------------------------------------
+
 root.title("Conversor de temperatura: ") #Le da un titulo a la ventana
-root.config(width=400, height=400) # Le da una configuracion a la ventana, en este caso, los tamaños estandar.
+root.geometry("400x300") # Cambia el tamaño de la ventana principal.
+
+# ---------------------------------------------------------------------------
 
 etiqueta_temp_celsius = ttk.Label(text="Temperatura en ºC:") # Crea un widget que pone un texto.
 etiqueta_temp_celsius.place(x=20, y=20) # El .place lo posiciona en la ventana de manera ABSOLUTA.
@@ -31,10 +37,14 @@ boton_convertir = ttk.Button(text="Convertir", command=convertir_temp)
 #Command le asigna una funcion que se ejecuta cuando el boton es presionado
 boton_convertir.place(x=20, y=60) 
 
+# ---------------------------------------------------------------------------
+
 etiqueta_temp_kelvin = ttk.Label(text="Temperatura en K: n/a")
 etiqueta_temp_kelvin.place(x=20, y=120)
 
 etiqueta_temp_fahrenheit = ttk.Label(text="Temperatura en ºF: n/a")
 etiqueta_temp_fahrenheit.place(x=20, y=160)
+
+# ---------------------------------------------------------------------------
 
 root.mainloop() # Muestra la ventana al usuario. Siempre va al final.
